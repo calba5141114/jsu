463b793396302228c7fddce6fa7b52696e756fc4
calba5141114 <CARLOSalba989@gmail.com>
2018-05-06 12:38:21 -0700

    responsiveness

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,7 +6,7 @@ sr.reveal('.bar');
 
 /* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("myNav").style.width = "40%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
