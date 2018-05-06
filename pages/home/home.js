ecbb99146545ce3c4ccb6f00eef12b0f40bb2f5e
calba5141114 <CARLOSalba989@gmail.com>
2018-05-06 13:11:25 -0700

    alerts

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,12 +1,30 @@
+// jshint esversion:6
+
+
+
 window.sr = ScrollReveal();
 sr.reveal('.foo');
 sr.reveal('.bar');
+
+window.onload = () => {
+    iziToast.show({
+        title: 'Hello!',
+        message: 'Would you mind taking a type form <a href="#" target="_blank"> survey? <a>   ',
+        position: 'topRight',
+        theme: 'dark'
+    });
+}
+
+
+
 
 
 
 /* Open when someone clicks on the span element */
 function openNav() {
     document.getElementById("myNav").style.width = "40%";
+
+
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
