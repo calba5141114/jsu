a2f544b03c20057c55f50e25a0ec5b4b81acbf8a
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-09-24 21:22:29 -0700

    improving news fetching

--- pages/IsNews/news.js
+++ pages/IsNews/news.js
@@ -1,5 +1,6 @@
-//jshint esversion:6
+
 const newsContainer = document.getElementById('newsContainer');
+
 const url = 'https://newsapi.org/v2/top-headlines?' +
     'country=il&' +
     'apiKey=3b229e5fa8824687b484a6b42baac102';
@@ -8,31 +9,39 @@ window.sr = ScrollReveal();
 sr.reveal('.foo');
 sr.reveal('.bar');
 
-
-
-
-
 const backHome = document.getElementById('backHome');
 
-var request = new XMLHttpRequest();
-request.open("GET", url);
-request.responseType = "json";
-request.send();
-request.onload = resp => {
-    FmtAndPrnt(resp);
-};
 
-/**
- * @param {json} jsonFile - json data we get from the news api
- * This function formats the json and pushes it to the HTML template.
- */
+// fetching articles from the news api
+fetch(url)
+    .then(data => data.json())
+    .then(fmtData => FmtAndPrnt(fmtData))
+
+
 function FmtAndPrnt(jsonFile) {
+
     // Temporary holding place for our Txt
-    var printData = " ";
-    let list = jsonFile.target.response.articles;
+    let printData = " ";
+    let list = jsonFile.articles;
+    const totalResults = jsonFile.totalResults;
+
+
+    let articleCount = document.getElementById('articleCount');
+
+    articleCount.innerHTML = ` <p>${totalResults}</p>  `;
+
+
+    // printing out and formatiting all the recieved articles.
 
     for (let index in list) {
+
+
+        if(list[index].urlToImage <= 0){
+            list[index].urlToImage = '';
+        }
+
         var newsCard = `
+
 <div class="newsCard foo">
 <h5> Source: ${list[index].source.name},
 ${list[index].author}</h5>
@@ -44,10 +53,12 @@ ${list[index].author}</h5>
  <h5>
 Published: ${list[index].publishedAt}</h5>
 <div>
-`;
 
+`;
         printData += newsCard;
+
     }
+
     newsContainer.innerHTML = printData;
 }
 
